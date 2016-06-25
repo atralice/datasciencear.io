942e5b7d36890d9fadfa7ec8ef0c5fde653abef7
Toni Tralice <atralice@gmail.com>
2016-06-25 12:48:04 -0300

    logo colores

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', pagina: 'home/landing' });
+  res.render('index', { title: 'Data Science Ar', pagina: 'home/landing' });
 });
 
 module.exports = router;
